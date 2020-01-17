fae3bee56929aa24d1b6d99aa0835969b7eb006f
Raka Dean <rakache@gmail.com>
2020-01-17 15:59:51 +0700

    add link to web&call

--- Calculate/app/src/main/java/com/aditas/calculate/MenuActivity.java
+++ Calculate/app/src/main/java/com/aditas/calculate/MenuActivity.java
@@ -3,11 +3,13 @@ package com.aditas.calculate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -16,7 +18,7 @@ import static com.aditas.calculate.MainActivity.EXTRA_NAME;
 
 public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnCalc, btnExit;
+    Button btnCalc, btnExit, btnCall, btnYt;
     TextView tvNm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +26,13 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_menu);
 
         btnCalc = findViewById(R.id.btn_calc);
+        btnCall = findViewById(R.id.btn_call);
+        btnYt = findViewById(R.id.btn_yt);
         btnExit = findViewById(R.id.btn_exit);
         tvNm = findViewById(R.id.tv_nm);
         btnExit.setOnClickListener(this);
+        btnCall.setOnClickListener(this);
+        btnYt.setOnClickListener(this);
         btnCalc.setOnClickListener(this);
         //receiving data from intent
         //If(get.Intent().getExtras() != null){
@@ -50,6 +56,30 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_exit:
                 Snackbar.make(view, "Ketipuu", Snackbar.LENGTH_LONG).show();
+                break;
+            case R.id.btn_call:
+                String noHP = "08111856530";
+                Uri tel = Uri.parse("tel:"+noHP);
+
+                Intent dialContact = new Intent(Intent.ACTION_DIAL, tel);
+                //check if there is activity /apps that can handle this intent
+                if(dialContact.resolveActivity(getPackageManager()) != null){
+                    startActivity(dialContact);
+                }
+                else{
+                    Toast.makeText(this, "Can't make a call", Toast);
+                }
+                break;
+            case R.id.btn_yt:
+                String web = "www.youtube.com";
+                Uri dom = Uri.parse("http://"+web);
+
+                Intent openWeb = new Intent(Intent.ACTION_VIEW, dom);
+                //create chooser apps to open this intent
+                Intent chooser = Intent.createChooser(openWeb, "Open with : ");
+                if(openWeb.resolveActivity(getPackageManager()) != null){
+                    startActivity(openWeb);
+                }
                 break;
         }
     }
