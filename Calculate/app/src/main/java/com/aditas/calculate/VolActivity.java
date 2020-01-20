04605089f4e6c5b39230a5c53ee5b0f20aae597a
Raka Dean <rakache@gmail.com>
2020-01-20 17:00:39 +0700

    tambahin sharedreferences di calc

--- Calculate/app/src/main/java/com/aditas/calculate/VolActivity.java
+++ Calculate/app/src/main/java/com/aditas/calculate/VolActivity.java
@@ -2,23 +2,28 @@ package com.aditas.calculate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.SharedPreferences;
 
 public class VolActivity extends AppCompatActivity {
     private EditText etLength, etWidth, etHeight;
     private TextView tvHsl;
     private Button btnHtg;
     private double length, width, height, result;
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vol);
+
+        pref = getSharedPreferences("prefku", MODE_PRIVATE);
 
         init();
         btnHtg.setOnClickListener(new View.OnClickListener() {
@@ -29,16 +34,43 @@ public class VolActivity extends AppCompatActivity {
                 result = (length * width * height) / 2;
                 tvHsl.setText(String.valueOf(result));
 
-                resetText();
             }
         });
+        getPref();
     }
+
+    private void setPref(){
+        SharedPreferences.Editor editor = pref.edit();
+        String l = etLength.getText().toString();
+        String h = etHeight.getText().toString();
+        String w = etWidth.getText().toString();
+        String r = tvHsl.getText().toString();
+
+        editor.putString("length", l);
+        editor.putString("height", h);
+        editor.putString("width", w);
+        editor.putString("result", r);
+        editor.apply();
+    }
+
     private void init(){
         etLength = findViewById(R.id.et_pjg);
         etWidth = findViewById(R.id.et_lbr);
         etHeight = findViewById(R.id.et_tgg);
         tvHsl = findViewById(R.id.tv_hsl);
         btnHtg = findViewById(R.id.btn_htg);
+    }
+
+    private void getPref(){
+        String length = pref.getString("length", "");
+        String height = pref.getString("height", "");
+        String width = pref.getString("width", "");
+        String result = pref.getString("result", "");
+
+        etLength.setText(length);
+        etHeight.setText(height);
+        etWidth.setText(width);
+        tvHsl.setText(result);
     }
 
     private void getText(){
@@ -53,6 +85,7 @@ public class VolActivity extends AppCompatActivity {
             height = Double.valueOf(stHeight);
             width = Double.valueOf(stWidth);
             length = Double.valueOf(stLength);
+            setPref();
         }
     }
 
@@ -61,4 +94,9 @@ public class VolActivity extends AppCompatActivity {
         etWidth.setText("");
         etHeight.setText("");
     }
+
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//    }
 }
